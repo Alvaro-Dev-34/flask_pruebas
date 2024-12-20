9a4f37d29a0e46fc32121d732fbf3b37916106ff
Alvaro-Dev-34 <alvaro.rivassolera@gmail.com>
2024-12-20 17:10:08 +0100

    cambio la forma de asignar el puerto a la app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import http
+import os
 
 from flask import Flask, jsonify, request
 
@@ -44,4 +45,5 @@ def create_user():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port, debug=False)
